57010d91f95f3214ea6e469a375664a2c9a14828
Oliver <rosaraguro@utpl.edu.ec>
2023-06-15 11:57:10 -0500

    Clase 02

--- EjemploHerencia02/HerenciaUno/src/paquete6/Ejecutor2.java
+++ EjemploHerencia02/HerenciaUno/src/paquete6/Ejecutor2.java
@@ -5,6 +5,7 @@
  */
 package paquete6;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -12,35 +13,65 @@ import java.util.Scanner;
  * @author reroes
  */
 public class Ejecutor2 {
+
     public static void main(String[] args) {
         // Ingresar estudiante de tipo presencial por teclado.
         // El usuario decide cuando terminar
         // No utilizar super.toString en EstudiantePresencial
         // Debe existir 1 sola impresión de todos los estudiantes presenciales
         // No usar arreglos
-        
-        // Trabajo clases 16 junio 2022
+
+        // Trabajo clases 15 junio 2023
         Scanner entrada = new Scanner(System.in);
         boolean bandera = true;
         String cadena = "";
-        
-        while(bandera){
-            System.out.println("ingresa un saludo");
-            String saludo = entrada.nextLine();
-            cadena = String.format("%s%s\n", cadena, saludo);
-            System.out.println("Desea salir, pulse S");
+
+        while (bandera) {
+            System.out.println("Ingrese nombre");
+            String n = entrada.nextLine();
+            System.out.println("Ingrese apellido");
+            String ap = entrada.nextLine();
+            System.out.println("Ingrese identificación");
+            String ced = entrada.nextLine();
+            System.out.println("Ingrese edad");
+            int edad = entrada.nextInt();
+            System.out.println("Ingrese número de créditos");
+            int creditos = entrada.nextInt();
+            System.out.println("Ingrese costo de créditos");
+            double costo = entrada.nextInt();
+            System.out.println("-------------------------");
+
+            EstudiantePresencial presencial = new EstudiantePresencial(n, ap,
+                    ced, edad, costo, creditos);
+            presencial.calcularMatriculaPresencial();
+
+            entrada.nextLine();
+            cadena = String.format("%s"
+                    + "Nombre: %s\n"
+                    + "Apellido: %s\n"
+                    + "Identificación: %s\n"
+                    + "Edad: %s\n"
+                    + "NUmero Creditos: %d\n"
+                    + "CostoCreditos: %.2f\n"
+                    + "Valor Matricula: %.2f\n"
+                    + "--------------------------------",
+                    cadena,
+                    presencial.nombresEstudiante,
+                    presencial.apellidosEstudiante,
+                    presencial.identificacionEstudiante,
+                    presencial.edadEstudiante,
+                    presencial.obtenerNumeroCreditos(),
+                    presencial.obtenerCostoCredito(),
+                    presencial.obtenerMatriculaPresencial());
+            
+            System.out.println("Desea salir, ingrese SI");
             String opcion = entrada.nextLine();
-            if(opcion.equals("S")){
+            if (opcion.equals("SI")) {
                 bandera = false;
+
             }
         }
-        
         System.out.printf("%s\n", cadena);
-        
-        
+
     }
 }
-
-
-
-
